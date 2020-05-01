a0b2cae6e4f1e6fe109aca78de47cf0419a044d8
Juda <judaponce14@unitec.edu>
2020-05-01 13:58:54 -0600

    validando

--- P3_Lab1JudaPonc.cpp
+++ P3_Lab1JudaPonc.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include<math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
 int main(int argc, char** argv) {
+	int inputNumber;
+	cout<<" Por favor ingrese un numero menos que 100: ";
+	cin>> inputNumber;
+	if(inputNumber<=2 || inputNumber>=100){
+	
+	cout<<"Lo sentimos el numero debe ser mayor o igual que 2 o menor o igual que 99";
+}else{
+	cout<<"correcto";
+}
+	
 	return 0;
 }
