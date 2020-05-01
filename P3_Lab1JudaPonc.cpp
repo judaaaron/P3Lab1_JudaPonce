911694840756083f58b28b6d43c6814305485c87
Juda <judaponce14@unitec.edu>
2020-05-01 15:30:25 -0600

    terminando

--- P3_Lab1JudaPonc.cpp
+++ P3_Lab1JudaPonc.cpp
@@ -12,31 +12,32 @@ int main(int argc, char** argv) {
 		cout<<"Lo sentimos el numero debe ser mayor o igual que 2 o menor o igual que 99";
 			}else{
 				for(int i=0; i< inputNumber; i++){
-					auxiliar=i%2==0;
-		 			int ceros = 0, contadorPrimos = 1;// Se declara y se inicializa 2 contadores, uno que controla el ciclo y otro que controla la cantida de residuo que sean igual a 0
-            while (contadorPrimos <= i) {
-                if (i % contadorPrimos == 0) {
-                    ceros++;
-                }
-                contadorPrimos++;// Se incrementa este ciclo para que el ciclo no se haga infinito
-            }
-            if (ceros == 2) {// Se valida de que el contador de residuo igual a cero en total sea 2
-            	//for(int j=0;  j< inputNumber; j++){
 				
-               while(i+i==contador){
-               		cout<< i<<" + "<< i<< " = "<< contador<<endl;
-               		contador+=2;
-               		if(contador<inputNumber){
-               			break;
-					   }
-               		
-			  // }
-		}
-
+		 			int ceros = 0, ceros2=0, contadorPrimos = 1, contadorPrimos2=1;// Se declara y se inicializa 2 contadores, uno que controla el ciclo y otro que controla la cantida de residuo que sean igual a 0
+            		while (contadorPrimos <= i) {
+                		if (i % contadorPrimos == 0) {
+                    	ceros++;
+                		}
+                		contadorPrimos++;// Se incrementa este ciclo para que el ciclo no se haga infinito
+            		}
+            		for(int j=0; j< inputNumber; j++){
+            				while (contadorPrimos2 <= j) {
+                				if (i % contadorPrimos2 == 0) {
+                    			ceros2++;
+                		}
+                		contadorPrimos2++;// Se incrementa este ciclo para que el ciclo no se haga infinito
+            		}
+					
+            		if (ceros == 2 ||  ceros2==2) {// Se valida de que el contador de residuo igual a cero en total sea 2
+            		cout<<j<<endl;
+			        	
+               }
             } 
 		
-	}
-}
+				
+			
+		}
+		}
 	
 	return 0;
 }
